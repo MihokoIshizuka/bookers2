5e804347322fe0a34bfc3edb816dcae4629ec20f
EC2 Default User <ec2-user@ip-172-31-22-106.ec2.internal>
2022-04-20 07:25:26 +0000

    [add]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,6 +36,7 @@ class BooksController < ApplicationController
     def show
         @book = Book.find(params[:id])
         @user = User.find(@book.user_id)
+        @books = Book.new
     end
 
     def destroy
